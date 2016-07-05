f9252ea8d94006cf69c0bf4f4415f6a7291238cc
Darkmarty <unal.batuhan@outlook.com>
2016-07-05 23:20:21 +0300

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Captcha.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    Güvenlik Kodu : <asp:Image ID="imgKod" runat="server" />
+        <br />
+        <br />
+        Güvenlik Kodunu Girin : <asp:TextBox ID="txtkontrolKodu" runat="server"></asp:TextBox>  
+        <br />
+        <br />
+        <asp:Button ID="btnKontrol" runat="server" Text="Kontrol" OnClick="btnKontrol_Click" />  
+        <br />
+        <br />
+        <asp:Label ID="lblMesaj" runat="server" Text="Label"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Web.UI;
+
+
+namespace Captcha
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                ResimOlustur();
+            }
+        }
+        public void ResimOlustur()
+        {
+            string kod = "";
+            kod = RastgeleVeriUret();
+            Session.Add("kod", kod);
+            Bitmap bmp = new Bitmap(100, 21);
+            Graphics g = Graphics.FromImage(bmp);
+            g.DrawString(kod, new Font("Comic Sanns MS", 15), new SolidBrush(Color.Black),20,0);
+            MemoryStream ms = new MemoryStream();
+            bmp.Save(ms, ImageFormat.Png);
+            var base64data = Convert.ToBase64String(ms.ToArray());
+            imgKod.ImageUrl = "data:image/png;base64," + base64data;
+            g.Dispose();
+            bmp.Dispose();
+            ms.Close();
+            ms.Dispose();
+        }
+        public string RastgeleVeriUret()
+        {
+            string deger = "";
+            string dizi = "ABCDEFGHIJKLMNOPRSTUVYZ0123456789";
+            Random r = new Random();
+            for (int i = 0; i < 5; i++) 
+            {
+                deger = deger + dizi[r.Next(0, 33)];
+            }
+            return deger;
+        }
+
+        protected void btnKontrol_Click(object sender, EventArgs e)
+        {
+            if (txtkontrolKodu.Text == Session["kod"].ToString()) 
+            {
+                lblMesaj.Text = "Dogrulama Saglandı";
+            }
+            else
+            {
+                lblMesaj.Text = "Dogrulama Kodu Yanlıs";
+                ResimOlustur();
+            }
+          
+        }
+    }
+}
